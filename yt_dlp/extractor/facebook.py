f7475d2f0251ba2dc1fe6f98fdff09a1aed8e43d
kclauhk <78251477+kclauhk@users.noreply.github.com>
2023-12-28 23:35:28 +0800

    Update facebook.py

--- yt_dlp/extractor/facebook.py
+++ yt_dlp/extractor/facebook.py
@@ -982,8 +982,7 @@ class FacebookAdLibIE(InfoExtractor):
                             'description': entry.get('link_description') or description,
                         }
                         if (entry.get('watermarked_video_sd_url') or entry.get('video_sd_url')
-                            or entry.get('watermarked_video_hd_url') or entry.get('video_hd_url')
-                        ):
+                            or entry.get('watermarked_video_hd_url') or entry.get('video_hd_url')):
                             entries.append({**basic, **{
                                 'thumbnail': entry.get('video_preview_image_url'),
                                 'formats': extract_format(entry),
