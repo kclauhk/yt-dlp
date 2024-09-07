327f542b1dcbbfc8a8f94a1438834879485338cc
kclauhk <78251477+kclauhk@users.noreply.github.com>
2024-09-07 11:29:41 +0800

    [ie/piramidetv] Add extractor

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -1550,6 +1550,11 @@ from .pinterest import (
     PinterestCollectionIE,
     PinterestIE,
 )
+from .piramidetv import (
+    PiramideTVChannelIE,
+    PiramideTVChannelURLIE,
+    PiramideTVIE,
+)
 from .pixivsketch import (
     PixivSketchIE,
     PixivSketchUserIE,
