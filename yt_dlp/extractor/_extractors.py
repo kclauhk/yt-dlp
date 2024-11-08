5d898d7e2b1d706c0b84385a3fd072f89b3ff508
kclauhk <78251477+kclauhk@users.noreply.github.com>
2024-11-08 16:45:21 +0800

    [ie/idoltv] Add extractor

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -851,6 +851,12 @@ from .ichinanalive import (
     IchinanaLiveVODIE,
 )
 from .idolplus import IdolPlusIE
+from .idoltv import (
+    IdoltvIE,
+    IdoltvSearchIE,
+    IdoltvSearchURLIE,
+    IdoltvVodIE,
+)
 from .ign import (
     IGNIE,
     IGNArticleIE,
