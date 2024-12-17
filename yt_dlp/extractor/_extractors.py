dcec84da9147e910b2f6eaf776f73c70fc29e1d5
kclauhk <78251477+kclauhk@users.noreply.github.com>
2024-12-18 02:05:51 +0800

    [ie/extrememusic] Add extractor

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -614,6 +614,12 @@ from .europeantour import EuropeanTourIE
 from .eurosport import EurosportIE
 from .euscreen import EUScreenIE
 from .expressen import ExpressenIE
+from .extrememusic import (
+    ExtremeMusicAIE,
+    ExtremeMusicIE,
+    ExtremeMusicPIE,
+    ExtremeMusicSearchIE,
+)
 from .eyedotv import EyedoTVIE
 from .facebook import (
     FacebookAdsIE,
