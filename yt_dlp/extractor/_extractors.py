f164eaa051f0624ba37fc26d79d38dce9db4bf99
kclauhk <78251477+kclauhk@users.noreply.github.com>
2024-12-22 02:01:25 +0800

    [ie/meritplus] Add extractor

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -1158,6 +1158,7 @@ from .megatvcom import (
 )
 from .meipai import MeipaiIE
 from .melonvod import MelonVODIE
+from .meritplus import MeritPlusIE
 from .metacritic import MetacriticIE
 from .mgtv import MGTVIE
 from .microsoftembed import (
