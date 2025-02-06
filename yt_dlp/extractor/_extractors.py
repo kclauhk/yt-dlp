706ac53cd10de5dd55fc7af76cc4293b2f21a60d
kclauhk <78251477+kclauhk@users.noreply.github.com>
2025-02-06 23:51:20 +0800

    [ie/vmware] Add extractor

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -2408,6 +2408,11 @@ from .vk import (
     VKUserVideosIE,
     VKWallPostIE,
 )
+from .vmware import (
+    VMwareExploreIE,
+    VMwareIE,
+    VMwareSearchIE,
+)
 from .vocaroo import VocarooIE
 from .vodpl import VODPlIE
 from .vodplatform import VODPlatformIE
