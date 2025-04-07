c75bf55a252a2affc25394bfd6c0dc15a693c31e
kclauhk <78251477+kclauhk@users.noreply.github.com>
2025-04-07 09:29:25 +0800

    [ie/kidoodletv] Add extractor

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -978,6 +978,10 @@ from .kick import (
 )
 from .kicker import KickerIE
 from .kickstarter import KickStarterIE
+from .kidoodletv import (
+    KidoodleTVIE,
+    KidoodleTVSeriesIE,
+)
 from .kika import (
     KikaIE,
     KikaPlaylistIE,
