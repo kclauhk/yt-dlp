e44be10e9283d8150817c38ca541bf08e63bae83
kclauhk <78251477+kclauhk@users.noreply.github.com>
2025-08-28 10:28:59 +0800

    [ie/youtube] Revert changes
    - restore `ios` into default clients
    - change default playback_wait to 0
    - revert deprioritization of web_safari m3u8 formats

--- yt_dlp/extractor/youtube/_video.py
+++ yt_dlp/extractor/youtube/_video.py
@@ -257,7 +257,7 @@ class YoutubeIE(YoutubeBaseInfoExtractor):
         '401': {'ext': 'mp4', 'height': 2160, 'format_note': 'DASH video', 'vcodec': 'av01.0.12M.08'},
     }
     _SUBTITLE_FORMATS = ('json3', 'srv1', 'srv2', 'srv3', 'ttml', 'srt', 'vtt')
-    _DEFAULT_CLIENTS = ('tv_simply', 'tv', 'web')
+    _DEFAULT_CLIENTS = ('tv', 'ios', 'web')
     _DEFAULT_AUTHED_CLIENTS = ('tv', 'web_safari', 'web')
     # Premium does not require POT (except for subtitles)
     _DEFAULT_PREMIUM_CLIENTS = ('tv', 'web_creator', 'web')
@@ -3411,7 +3411,7 @@ class YoutubeIE(YoutubeBaseInfoExtractor):
         gvs_pots = {}
 
         # For handling potential pre-playback required waiting period
-        playback_wait = int_or_none(self._configuration_arg('playback_wait', [None])[0], default=6)
+        playback_wait = int_or_none(self._configuration_arg('playback_wait', [None])[0], default=0)
 
         for fmt in streaming_formats:
             client_name = fmt[STREAMING_DATA_CLIENT_NAME]
@@ -3644,10 +3644,6 @@ class YoutubeIE(YoutubeBaseInfoExtractor):
 
             if f.get('source_preference') is None:
                 f['source_preference'] = -1
-
-            # Deprioritize since its pre-merged m3u8 formats may have lower quality audio streams
-            if client_name == 'web_safari' and proto == 'hls' and live_status != 'is_live':
-                f['source_preference'] -= 1
 
             if missing_pot:
                 f['format_note'] = join_nonempty(f.get('format_note'), 'MISSING POT', delim=' ')
