abd0f16f629b30c32abd8ec44a80806c0c198b5f
kclauhk <78251477+kclauhk@users.noreply.github.com>
2026-05-11 10:17:13 +0800

    [ie/common] Add `_before_download`

--- yt_dlp/YoutubeDL.py
+++ yt_dlp/YoutubeDL.py
@@ -3307,6 +3307,9 @@ class YoutubeDL:
         else:
             params = self.params
 
+        if ie := traverse_obj(self._ies_instances, (..., {object}), get_all=False):
+            ie._before_download(info, subtitle=subtitle, test=test)
+
         fd = get_suitable_downloader(info, params, to_stdout=(name == '-'))(self, params)
         if not test:
             for ph in self._progress_hooks:
